b875feca5ba129cb5bc1fd3f8d6f0e50c473a7d4
munuhee <murichustephen@gmail.com>
2023-10-17 10:21:30 +0300

    update config.py

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ SECRET_KEY = 'your-secret-key'
 
 # Using an environment variable to determine which database URI to use
 if os.environ.get('FLASK_ENV') == 'testing':
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory SQLite for testing
 else:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:testing321@localhost/auth-service'
