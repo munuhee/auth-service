c5606bd1fd7011e5b604b91eabc5d615e5812b04
munuhee <murichustephen@gmail.com>
2023-10-19 11:06:43 +0300

    add refresh tokens

--- app/models.py
+++ app/models.py
@@ -10,5 +10,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    refresh_token = db.Column(db.String(200), nullable=True)
     def __str__(self):
         return f"User(id={self.id}, username='{self.username}')"
