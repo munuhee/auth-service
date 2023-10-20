8fc917a86759e816fb2acb1b55eb3ddeba44549a
munuhee <murichustephen@gmail.com>
2023-10-20 21:40:15 +0300

    WIP

--- app/services.py
+++ app/services.py
@@ -19,30 +19,31 @@ def generate_tokens(user):
 
     return access_token, refresh_token
 
-def register_user(username, password):
+def register_user(username, email, password):
     """Register a new user and store their information in the database."""
-    existing_user = User.query.filter_by(username=username).first()
+    existing_user = User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first()
+    
     if existing_user:
-        return {"message": "Username already exists"}, 400
+        return {"message": "Username or email already exists"}, 400
 
     hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
-    new_user = User(username=username, password=hashed_password)
+    new_user = User(username=username, email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
     access_token, refresh_token = generate_tokens(new_user)
     return {"message": "Registration successful", "access_token": access_token, "refresh_token": refresh_token}, 201
 
+
 def login_user(username, password):
     """Log in a user and return an access token upon successful login."""
-    user = User.query.filter_by(username=username).first()
+    user = User.query.filter((User.username == username) | (User.email == username)).first()
 
     if not user:
         return {"message": "User not found"}, 404
 
     if check_password_hash(user.password, password):
-        # Generate and return new access and refresh tokens
         access_token, refresh_token = generate_tokens(user)
         return {"message": "Login successful", "access_token": access_token, "refresh_token": refresh_token}, 200
     return {"message": "Invalid password"}, 401
