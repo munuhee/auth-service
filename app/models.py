999604e57b980c1b260ab9a116c9f4f3b1d88bcf
munuhee <murichustephen@gmail.com>
2023-10-23 20:23:50 +0300

    Implement password reset and change functionality

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 This module defines the User model for storing user information.
 It uses the SQLAlchemy database connection provided by the 'db' instance from the 'app' package.
 """
-
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -14,3 +14,18 @@ class User(db.Model):
     refresh_token = db.Column(db.String(200), nullable=True)
     def __str__(self):
         return f"User(id={self.id}, username='{self.username}')"
+
+class PasswordResetToken(db.Model):
+    """Password reset model"""
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    token = db.Column(db.String(100), unique=True, nullable=False)
+    expiration = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, user_id, token):
+        self.user_id = user_id
+        self.token = token
+
+    def is_expired(self):
+        """Check if the expiration timestamp has passed."""
+        return datetime.utcnow() > self.expiration
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,23 @@
-"""Routes for user registration and login.
-
-Keyword arguments:
-- health -- Check the health status of the application.
-- register -- Register a new user.
-- login -- Log in a user.
-Return: JSON responses with appropriate status codes.
 """
+Routes for User Registration, Login, and Password Reset
+
+This module defines the routes for user registration, login, and password reset
+functionality, providing RESTful endpoints for managing user accounts and
+password recovery.
+
+Endpoints:
+- /api/register: Register a new user.
+- /api/login: Log in a user.
+- /api/reset-password: Initiate a password reset, verify reset tokens, and reset passwords.
+
+Returns: JSON responses with appropriate status codes.
+"""
+
 from flask import request, jsonify
-from app import app
-from app.services import register_user, login_user
+from werkzeug.security import generate_password_hash
+from app import app, db
+from app.services import register_user, login_user, generate_reset_token, send_reset_email
+from app.models import PasswordResetToken, User
 
 @app.route('/api/status', methods=['GET'])
 def health():
@@ -33,3 +42,40 @@ def login():
     password = data.get('password')
     response, status_code = login_user(identifier, password)
     return jsonify(response), status_code
+
+@app.route('/api/reset-password', methods=['POST'])
+def request_password_reset():
+    """Initiate a password reset request."""
+    data = request.get_json()
+    email = data.get('email')
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return {"message": "User not found"}, 404
+
+    token = generate_reset_token(user)
+    send_reset_email(user, token)
+
+    return {"message": "Password reset email sent. Check your inbox."}, 200
+
+@app.route('/api/reset-password/<token>', methods=['POST'])
+def reset_password(token):
+    """Reset a user's password using a valid reset token."""
+    data = request.get_json()
+    new_password = data.get('password')
+
+    reset_token = PasswordResetToken.query.filter_by(token=token).first()
+
+    if not reset_token:
+        return {"message": "Invalid reset token"}, 400
+
+    if reset_token.is_expired():
+        return {"message": "Reset token has expired"}, 400
+
+    user = User.query.get(reset_token.user_id)
+    user.password = generate_password_hash(new_password, method='pbkdf2:sha256')
+    db.session.delete(reset_token)
+    db.session.commit()
+
+    return {"message": "Password reset successful"}, 200
